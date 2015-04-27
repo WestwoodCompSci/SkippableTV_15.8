f97a03d2283441bf16e53d3a04bd21913b15db8e
BrianCoombs <bjcoombs1@gmail.com>
2015-04-27 15:14:01 -0500

    User Class Added/Edited
    BCoombs wuz here

--- src/User.java
+++ src/User.java
@@ -1,4 +1,52 @@
+import java.awt.image.BufferedImage;
 
-public class User {
+
+
+public class User 
+{
+	private String myUsername;
+	private String myPassword;
+	private BufferedImage myPic;
 	
+	
+	public User(String username, String mypassword, BufferedImage mypic)
+	{
+		setMyUsername(username);
+		setMyPassword(mypassword);
+		setMyPic(mypic);
+		
+	}
+	
+	public String getMyUsername() 
+	{
+		return myUsername;
+	}
+	
+	public void setMyUsername(String myUsername) 
+	{
+		this.myUsername = myUsername;
+	}
+
+	
+	public String getMyPassword() {
+		return myPassword;
+	}
+
+	public void setMyPassword(String myPassword) 
+	{
+		this.myPassword = myPassword;
+	}
+
+	
+	public BufferedImage getMyPic() 
+	{
+		return myPic;
+	}
+	
+	public void setMyPic(BufferedImage myPic) 
+	{
+		this.myPic = myPic;
+	}
+	
+
 }
