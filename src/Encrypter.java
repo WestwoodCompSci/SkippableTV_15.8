4cfbde9e630a6ec8380d8d2f2343568c2d1236a7
garimasingh2016 <garimasingh@Garimas-MacBook-Air.local>
2015-04-28 14:23:40 -0500

    Better encryption yay!

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,34 +1,101 @@
-public class Encrypter {
-	
-	private static String myKey;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
-	public Encrypter(String key)
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class Encrypter {
+
+	private Cipher myCipher;
+	private SecretKey myKey;
+
+	public Encrypter() 
 	{
-		myKey = key;
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128);
+			myKey = kgen.generateKey();
+
+			myCipher = Cipher.getInstance("AES");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 
-	public String encrypt(String plainText)
-	{
-		String encoded = "";
-
-		for (int i = 0; i < plainText.length(); i++)
-		{
-			encoded += (char) (plainText.charAt(i) + myKey.charAt(i % myKey.length()));
+	public byte[] encrypt(String plainText)
+	{		
+		try {
+			myCipher.init(Cipher.ENCRYPT_MODE, myKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		byte[] input = plainText.getBytes();
 
-		return encoded;
+		byte[] encrypted = new byte[myCipher.getOutputSize(input.length)];
+		
+		try {
+			encrypted =  myCipher.doFinal(input);
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return encrypted;
 	} 
 
-	public String decrypt(String encoded)
+	public String decrypt(byte[] encoded)
 	{
-		String decoded = "";		
-		
-		for (int i = 0; i < encoded.length(); i++)
-		{
-			decoded += (char) (encoded.charAt(i) - myKey.charAt(i % myKey.length()));
+		try {
+			myCipher.init(Cipher.DECRYPT_MODE, myKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		byte[] decrypted = new byte[myCipher.getOutputSize(encoded.length)];
+		try {
+			decrypted =  myCipher.doFinal(encoded);
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return decoded;
+		return this.bytesToString(decrypted);
+	}
+
+	private String bytesToString(byte[] b)
+	{
+		String f = "";
+		
+		for (byte c : b)
+		{
+			f += (char) c;
+		}
+		
+		return f;
+	}
+	
+	public static void main(String[] args)
+	{
+		Encrypter e = new Encrypter();
+		byte[] enc =  e.encrypt("Hello world");
+		System.out.println(e.decrypt(enc));
 	}
 }
 
