a98a250e7f781b3c9f8b5228950471042d2e3a2b
garimasingh2016 <garimasingh@Garimas-MacBook-Air.local>
2015-04-29 15:23:17 -0500

    Added comments
    Cuz Mickel made me X-(

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -45,6 +45,7 @@ public class Encrypter {
 		//Initializes a byte array that will store the encrypted bytes
 		byte[] encrypted = new byte[myCipher.getOutputSize(input.length)];
 		
+		//Encrypts
 		try {
 			encrypted =  myCipher.doFinal(input);
 		} catch (IllegalBlockSizeException e) {
@@ -55,21 +56,25 @@ public class Encrypter {
 			e.printStackTrace();
 		}
 		
+		//Encrypted byte array --> to Base64 String
 		return Base64.getEncoder().encodeToString(encrypted);
 	} 
 
 	public String decrypt(String encoded)
 	{
+		//Initializes Cipher to decrypt mode
 		try {
 			myCipher.init(Cipher.DECRYPT_MODE, myKey);
 		} catch (InvalidKeyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		//Takes encoded Base64 String --> byte array
 		byte[] byteArr = Base64.getDecoder().decode(encoded);
-		
+		//Initializes byte array that will store decrypted array
 		byte[] decrypted = new byte[myCipher.getOutputSize(byteArr.length)];
+		
+		//Decrypts
 		try {
 			decrypted =  myCipher.doFinal(byteArr);
 		} catch (IllegalBlockSizeException e) {
@@ -80,9 +85,11 @@ public class Encrypter {
 			e.printStackTrace();
 		}
 		
+		//Returns decrypted byte array --> String
 		return this.bytesToString(decrypted);
 	}
-
+	
+	//Converts decrypted byte array to a String
 	private String bytesToString(byte[] b)
 	{
 		String f = "";
