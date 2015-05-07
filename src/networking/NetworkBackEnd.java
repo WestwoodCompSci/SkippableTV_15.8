cfd66d6c16d2feaa7945512e280720c991ada9b2
Michael Darmawan <darmawan.mori@gmail.com>
2015-05-07 14:14:28 -0500

    BAckend Prog

--- src/networking/NetworkBackEnd.java
+++ src/networking/NetworkBackEnd.java
@@ -1,5 +1,17 @@
 package networking;
 
+/**This is a class that is going to be used with the Network Backend.
+ * @author Michael Darmawan
+ *
+ */
 public class NetworkBackEnd {
+	//BackEnd b;
+	
 	//TODO:Parse request String and return appropriate object.
+	public static void interpret(String cmd){
+		String[] prt=cmd.split(NetworkClient.delim);
+		if(prt[0].equals("")){
+			
+		}
+	}	
 }
