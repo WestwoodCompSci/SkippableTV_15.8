b0c02ac3f4c20a93001fec755963d98872a3bce7
Zahin Nambiar <s511383@student.roundrockisd.org>
2015-05-07 14:45:32 -0500

    Revert "GUI V3"
    This reverts commit 00f41ae66ae2895b72457a08618dfafd402e5105.

--- src/GUI/ButtonMod.java
+++ src/GUI/ButtonMod.java
@@ -2,45 +2,20 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 
-import javax.swing.AbstractButton;
-import javax.swing.ButtonModel;
 import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.plaf.basic.BasicButtonUI;
 
 
 
+@SuppressWarnings("serial")
+public class ButtonMod extends JButton {
 
-
-
-class ButtonMod extends JButton{
-	
-	
-	public ButtonMod(String text)
-	{
-		
-		super(text);
-		
-		
-	}
-@Override
-	public void paint(Graphics g) {
-       
-        
-        g.setColor(Color.red);
-       
-        g.fillRect(this.getX(), getY(), getWidth(), getHeight());
-        
-        
-//public ButtonMod (String text, int i, int e, int u) {
-	//this.setText(text);
-	 //this.setBackground(new Color(i,e,u));
+public ButtonMod (String text, int i, int e, int u) {
+	 JButton a = new JButton(text);
+	 a.setBackground(new Color(i,e,u));
 	 //a.setForeground(Color.WHITE);
-	 //a.setFont(new Font("Tohoma", Font.BOLD, 13));
-	 //this.setFocusPainted(true);
-	 //a.setFont(new Font("Tohoma", Font.BOLD, 13));
+	 a.setFocusPainted(false);
+	 a.setFont(new Font("Tohoma", Font.BOLD, 13));
 	 //if ( t == true)
 	 //{
 		// active listener
@@ -55,11 +30,7 @@ class ButtonMod extends JButton{
 	 //}
 	 
 	 
-	}
-	
-
-
-}
+	}}
 /*you need to finish code, to fit the needs of the buttons 
 that you will  be producing with this constructor*/
 
