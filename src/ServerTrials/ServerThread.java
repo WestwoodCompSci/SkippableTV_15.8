05996ca91391bb0dff0465a05c25ab5f9f2a35b6
Michael Darmawan <darmawan.mori@gmail.com>
2015-05-13 15:39:46 -0500

    ServerThread shorter

--- src/ServerTrials/ServerThread.java
+++ src/ServerTrials/ServerThread.java
@@ -21,43 +21,4 @@ public class ServerThread extends Thread {
 		}
 	}
 
-	public ServerThread() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(Runnable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(ThreadGroup arg0, Runnable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(ThreadGroup arg0, String arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(Runnable arg0, String arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(ThreadGroup arg0, Runnable arg1, String arg2) {
-		super(arg0, arg1, arg2);
-		// TODO Auto-generated constructor stub
-	}
-
-	public ServerThread(ThreadGroup arg0, Runnable arg1, String arg2, long arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
-
 }
