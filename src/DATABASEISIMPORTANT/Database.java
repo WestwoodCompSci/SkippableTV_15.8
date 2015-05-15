38a7e5864fce7e6e4a181c613d059ab4e24a165a
Andrew McAdams <andmcadams@gmail.com>
2015-05-15 15:23:23 -0500

    Updated database class

--- src/DATABASEISIMPORTANT/Database.java
+++ src/DATABASEISIMPORTANT/Database.java
@@ -13,52 +13,52 @@ import Backend.*;
 
 public class Database 
 {
-	
+
 	static final String userFolder = "users";
 	static final String showFolder = "shows";
 	static String userDirectory = "";
 	static final String delimiter = "|||";
-	
+
 	public User getUser(String username, String password)
 	{
-		
+
 		userDirectory = (Database.userFolder + File.pathSeparator + username);
-		
+
 		if (new File(userDirectory).exists())
 		{
-			
+
 			if (readFile(new File(userDirectory + File.pathSeparator + "pswd.txt")).get(0).equals(password))
 			{
-				
+
 				try 
 				{
-					
+
 					URL picLoc = new URL("http://www.skippable.tv/" + userDirectory + File.pathSeparator + "pic.jpg");
-					
+
 					return new User(username, picLoc);
-					
+
 				}
-				
+
 				catch (Exception e) 
 				{ 
-					
+
 					System.out.println("there is a disturbance in the force"); 
-					
+
 					return null;
-					
+
 				}
-				
-				
+
+
 			}
-			
+
 		}
-			
+
 		return null;
-		
+
 	}
-	
-	
-	
+
+
+
 	/**
 	 * 
 	 * Not 100% sure this actually creates folders... :)
@@ -66,7 +66,7 @@ public class Database
 	 * @param password
 	 * @return
 	 */
-	
+
 	public boolean createUser(User newUser, String password)
 	{
 		try {
@@ -83,68 +83,85 @@ public class Database
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	public boolean updateUser(User updated, String password)
 	{
-		
+		deleteUser(updated.getMyUsername(), password);
+		createUser(updated, password);
 		return false;
-		
+
 	}
-	
+
 	public boolean deleteUser(String username, String password)
 	{
 		if(password.equals(readFile(new File(userDirectory + File.separator + "pswd.txt"))))
-			{
-				(new File(userDirectory)).delete();
-				return true;
-			}
-		
+		{
+			(new File(userDirectory)).delete();
+			return true;
+		}
+
 		return false;
-		
+
 	}
-	
-	public ArrayList<Season> getSeries()
+
+	public ArrayList<Show> getSeries()
 	{
-		
-		return new ArrayList<Season>();
-		
+
+		ArrayList<Show> series = new ArrayList<Show>();
+		boolean stillShows = true;
+		int counter = 0;
+		while(stillShows)
+		{
+			String show = "" + counter;
+			while(show.length() < 4)
+				show = "0" + show;
+			if(new File(Database.showFolder + File.pathSeparator + show).exists())
+			{
+				ArrayList<String> data = readFile(new File(Database.showFolder + File.pathSeparator + show + File.pathSeparator + "info.txt"));
+				series.add(new Show(data.get(0)));
+			}
+			else
+				stillShows = false;
+		}
+		return series;
+
 	}
-	
+
 	public ArrayList<String> readFile(File target)
 	{
-		
+
 		String line;
-		
+
 		ArrayList<String> lines = new ArrayList<String>();
-		
+
 		try 
 		{
-		
+
 			BufferedReader reader = new BufferedReader(new FileReader(target));
-			
+
 			while ((line = reader.readLine()) != null)
-				
+
 				lines.add(line);
-			
+
 			reader.close();
-		
+
 		}
-		
+
 		catch (Exception e) 
 		{ 
-			
+
 			System.out.println("Error"); 
-			
+
 			return null;
-			
+
 		}
-		
+
 		return lines;
-		
+
 	}
 
 }
