33c8f325bc4adfee71827463f7949a128351dedf
Zahin Nambiar <s511383@student.roundrockisd.org>
2015-05-15 15:15:49 -0500

    GUI V.10

--- src/GUI/GUI_selection.java
+++ src/GUI/GUI_selection.java
@@ -9,8 +9,8 @@ public class GUI_selection {
 	public Box Mainbox2;
 	public GUI_selection()
 	{
-		//JFrame frame1 = new JFrame();
-		//frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame1 = new JFrame();
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Box Mainbox2 = Box.createVerticalBox();
 		Box box1 = Box.createHorizontalBox();
 		Box box2 = Box.createHorizontalBox();
@@ -20,8 +20,8 @@ public class GUI_selection {
 		Mainbox2.add(box1);
 		Mainbox2.add(box2);
 
-		//frame1.pack();
-		//frame1.setVisible(true);
+		frame1.pack();
+		frame1.setVisible(true);
 		Mainbox2.setSize(1000,800);
 		
 		
